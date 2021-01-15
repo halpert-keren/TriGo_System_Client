ae900ce04918f3020eb656833ef41ba37ad5f3d6
halpert-keren <halpertkeren@gmail.com>
2021-01-15 10:02:08 +0200

    first components

--- src/index.js
+++ src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import TriGo from './Components/TriGo';
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <TriGo />
   </React.StrictMode>,
   document.getElementById('root')
 );
